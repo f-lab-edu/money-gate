929c565e42567adf370282b4b1f786335e2389d2
이준희 <juni416@naver.com>
2024-09-15 21:40:46 +0900

    feature: init-project

---
+++ src/test/java/com/joonhee/moneygate/MoneyGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joonhee.moneygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
