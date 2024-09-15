eb202aced2fa0032da9ecede5cba1ffaab0d31ca
이준희 <51898482+Marc416@users.noreply.github.com>
2024-09-15 21:57:28 +0900

    프로젝트 세팅

---
+++ src/main/java/com/joonhee/moneygate/MoneyGateApplication.java
@@ -0,0 +1,13 @@
+package com.joonhee.moneygate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyGateApplication.class, args);
+    }
+
+}
