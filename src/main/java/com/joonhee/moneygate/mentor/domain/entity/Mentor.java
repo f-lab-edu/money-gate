f79284e0ab99df1f9c42ca16c5159ae498d80864
이준희 <juni416@naver.com>
2024-09-30 15:24:20 +0900

    feature : Mentor Id 어노테이션붙이기

--- src/main/java/com/joonhee/moneygate/mentor/domain/entity/Mentor.java
+++ src/main/java/com/joonhee/moneygate/mentor/domain/entity/Mentor.java
@@ -1,7 +1,10 @@
 package com.joonhee.moneygate.mentor.domain.entity;
 
+import org.springframework.data.annotation.Id;
+
 public class Mentor {
-    private final Long id = 1L;
+    @Id
+    private Long id;
     private final String email;
     private final String nickName;
     private final String profileImage;
