871de42bb84e8459b223843ca2c1d77a27c143c0
이준희 <juni416@naver.com>
2024-10-13 01:04:50 +0900

    feature : Jdbc Config qusrud

--- src/main/java/com/joonhee/moneygate/configure/JdbcConfig.java
+++ src/main/java/com/joonhee/moneygate/configure/JdbcConfig.java
@@ -3,22 +3,19 @@ package com.joonhee.moneygate.configure;
 
 import com.joonhee.moneygate.jdbcconverter.account.JsonToRolesReadingConverter;
 import com.joonhee.moneygate.jdbcconverter.account.RolesToJsonWritingConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-public class JdbcConfig {
-
-    @Bean
-    public JdbcCustomConversions jdbcCustomConversions() {
-        return new JdbcCustomConversions(
-            Arrays.asList(
-                new JsonToRolesReadingConverter(),
-                new RolesToJsonWritingConverter()
-            )
+public class JdbcConfig extends AbstractJdbcConfiguration {
+    @Override
+    protected List<?> userConverters() {
+        return Arrays.asList(
+            new JsonToRolesReadingConverter(),
+            new RolesToJsonWritingConverter()
         );
     }
 }
